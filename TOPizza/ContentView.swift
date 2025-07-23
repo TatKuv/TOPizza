21e24e7514178dcaf053b04574c1fddb00677a00
TatKuv <ttatiana.s.mails@gmail.com>
2025-07-23 18:18:59 +0400

    Initial Commit

---
+++ TOPizza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TOPizza
+//
+//  Created by Tatiana Kuvarzina on 23.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TOPizza/TOPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOPizzaApp.swift
+//  TOPizza
+//
+//  Created by Tatiana Kuvarzina on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TOPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
