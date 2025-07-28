ff3e57a3c503b41248d494a6a7d1506c55955dc8
TatKuv <ttatiana.s.mails@gmail.com>
2025-07-29 01:35:00 +0400

    Added login status banner indecator of checking login and password

---
+++ LoginStatusBanner.swift
@@ -0,0 +1,18 @@
+//
+//  LoginStatusBanner.swift
+//  TOPizza
+//
+//  Created by Tatiana Kuvarzina on 28.07.2025.
+//
+
+import SwiftUI
+
+struct LoginStatusBanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginStatusBanner()
+}
